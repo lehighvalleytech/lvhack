4e4523864c4b1d6efbfcda4ea0a10d4524eca2b3
Tim Lytle <tim@timlytle.net>
2013-02-16 22:46:50 -0500

    Last relative link?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function HackerStuff($scope, $http) {
-  $http.get('/data.json').success(function(data) {
+  $http.get('./data.json').success(function(data) {
     $scope.hack = data.hack;
     console.log(data.hack);
   });
