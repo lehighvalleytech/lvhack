ea7e29b3b3550afcab1b605dd216f5cec60c6fe1
Tim Lytle <tim@timlytle.net>
2013-03-15 01:56:28 -0400

    Added Logos/Lame Attempt to Center

--- app.js
+++ app.js
@@ -3,4 +3,23 @@ function HackerStuff($scope, $http) {
     $scope.hack = data.hack;
     console.log(data.hack);
   });
+  
+  $scope.offset = function(index, total, span){
+	if((total*span - ((index+1)*span)) > 12){
+	  return; //not last line
+	}
+	
+	//only offset the first item of the last line
+	if((index)%(12/span)){
+		return;
+	}
+	
+	//find empty spans
+	var empty = total%(12/span);
+	if(empty == 0){
+	  return;
+	}
+	
+	return 'offset'+((12-(empty*span))/2);
+  };
 }
